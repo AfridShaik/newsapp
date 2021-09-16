7ac8918a8299575ec6738d566dabf87aa3bc82ad
AfridShaik <afridali102@gmail.com>
2021-09-17 00:20:22 +0530

    added infinate scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
   static defaultProps = {
@@ -27,6 +28,7 @@ export default class News extends Component {
       articles: [],
       loading: false,
       page: 1,
+      totalResults : 0
     };
     document.title = `${this.capitalizeFirstLetter(this.props.category)} -NewsHorse`;
   }
@@ -40,7 +42,7 @@ export default class News extends Component {
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading: false,
+      loading: true,
     });
   }
 
@@ -82,31 +84,62 @@ export default class News extends Component {
     this.setState({ page: this.state.page + 1 });
     this.updateNews();
   };
+
+  fetchMoreData = async() => {
+    this.setState({page : this.state.page + 1})
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d50a3d614e834f15af7b0681e3483bf0
+    &page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  };
+
+
+
+
   render() {
     return (
-      <div className="container my-4">
+      <>
         <h2 className="text-center" style={{ margin: "30px 0px" }}>
           News Lion - Top Headlines on {this.capitalizeFirstLetter(this.props.category)}
         </h2>
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <Newsitem
-                  title={element.title ? element.title : ""}
-                  description={element.description ? element.description : ""}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                  author={element.author}
-                  date={element.publishedAt}
-                  source={element.source.name}
-                />
-              </div>
-            );
-          })}
-        </div>
-        <div className="container d-flex justify-content-between">
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults }
+          loader={<Spinner/>}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <Newsitem
+                      title={element.title ? element.title : ""}
+                      description={element.description ? element.description : ""}
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+          </div>
+        </InfiniteScroll>
+
+
+
+
+        {/* <div className="container d-flex justify-content-between">
           <button
             disabled={this.state.page <= 1}
             type="button"
@@ -125,9 +158,9 @@ export default class News extends Component {
             onClick={this.handleNextClick}
           >
             Next &rarr;
-          </button>
-        </div>
-      </div>
+          </button> */}
+        {/* </div> */}
+      </>
     );
   }
 }
